7d17f051c909b59ec44bed122cf96d60c0ccc7ac
rihat99 <mr.rihat@gmail.com>
2023-11-11 00:01:05 +0400

    add mobilenetv3

--- model.py
+++ model.py
@@ -4,6 +4,9 @@ from torchvision.models import ResNet50_Weights
 from torchvision.models import resnet18
 from torchvision.models import ResNet18_Weights
 
+from torchvision.models import mobilenet_v3_small
+from torchvision.models import MobileNet_V3_Small_Weights
+
 from torch import nn
 import torch
 
@@ -41,7 +44,7 @@ def get_resnet50(pretrained=False):
 
 def get_resnet18(pretrained=False):
     if pretrained:
-        model = resnet18(weights=ResNet18_Weights.IMAGENET1K_V2)
+        model = resnet18(weights=ResNet18_Weights.IMAGENET1K_V1)
 
         # Freeze model weights
         for param in model.parameters():
@@ -70,10 +73,44 @@ def get_resnet18(pretrained=False):
     return model
 
 
+def get_mobilenet_v3_small(pretrained=False):
+    if pretrained:
+        model = mobilenet_v3_small(weights=MobileNet_V3_Small_Weights.IMAGENET1K_V1)
+
+        # Freeze model weights
+        for param in model.parameters():
+            param.requires_grad = False
+
+    else:
+        model = mobilenet_v3_small()
+
+    # Add on fully connected layers for the output of our model
+
+    model.fc = torch.nn.Sequential(
+        torch.nn.Linear(
+            in_features=576,
+            out_features=512,
+            bias=True
+        ),
+        torch.nn.ReLU(),
+        torch.nn.Dropout(p=0.5),
+        torch.nn.Linear(
+            in_features=512,
+            out_features=68 * 2,
+            bias=True
+        )
+    )
+
+    return model
+
+
 def get_model(model_name, pretrained=False):
     if model_name == "ResNet50":
         return get_resnet50(pretrained)
     elif model_name == "ResNet18":
         return get_resnet18(pretrained)
+    elif model_name == "MobileNetV3Small":
+        return get_mobilenet_v3_small(pretrained)
+    
     else:
         raise Exception("Model not implemented")
